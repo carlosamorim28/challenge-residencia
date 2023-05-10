9f2ef8fcbab64e169b15a5aae92e21ffd236ae42
Carlos Jorge <carlosjorge26111999@gmail.com>
2023-05-10 20:24:06 -0300

    DATABASE adapter

---
+++ armazenador de senhas/adapters/DatabaseAcapters.swift
@@ -0,0 +1,14 @@
+//
+//  DatabaseAcapters.swift
+//  armazenador de senhas
+//
+//  Created by userext on 10/05/23.
+//
+
+import Foundation
+
+protocol DatabaseAdapter {
+    func loadData() -> [Register]
+    func saveRegisterList(registers: [Register]) -> Bool
+    func getLastIdRegistred() -> Int
+}
---
+++ armazenador de senhas/adapters/FilemanagerAdapter.swift
@@ -0,0 +1,24 @@
+//
+//  FilemanagerAdapter.swift
+//  armazenador de senhas
+//
+//  Created by userext on 10/05/23.
+//
+
+import Foundation
+
+final class FilemanagerAdapter: DatabaseAdapter {
+    func loadData() -> [Register] {
+        FileManager.createFile(<#T##self: FileManager##FileManager#>)
+    }
+    
+    func saveRegisterList(registers: [Register]) -> Bool {
+        <#code#>
+    }
+    
+    func getLastIdRegistred() -> Int {
+        <#code#>
+    }
+    
+    
+}
