58f50e02c62c0ee57a475e40bb77813b741b2710
Carlos Jorge <carlosjorge26111999@gmail.com>
2023-05-11 20:57:58 -0300

    salvando local

--- exemplo-de-codigo.swift
+++ exemplo-de-codigo.swift
@@ -6,23 +6,23 @@ let sei = String(int)
 let sei2 = String(int2)
 
 
-struct FileHelper {
-	private let manager = FileManager.default
-	
-	func createDocument(named fileName: String, with data: Data)-> Bool {
-		let documentURL = manager.urls(for: .documentDirectory, in: .userDomainMask).first!
-		let url = documentURL.appendingPathComponent(fileName)
-		manager.createFile(atPath: url.path, contents: data)
-		print(documentURL)
-		return manager.fileExists(atPath: url.path)
-	}
-
-	func readDocument(at path: String) -> Data? {
-		let documentURL = manager.urls(for: .documentDirectory, in: .userDomainMask).first!
-		let url = documentURL.appendingPathComponent(path)
-		return try? Data(contentsOf: url)
-	}
-}
+//struct FileHelper {
+//	private let manager = FileManager.default
+//	
+//	func createDocument(named fileName: String, with data: Data)-> Bool {
+//		let documentURL = manager.urls(for: .documentDirectory, in: .userDomainMask).first!
+//		let url = documentURL.appendingPathComponent(fileName)
+//		manager.createFile(atPath: url.path, contents: data)
+//		print(documentURL)
+//		return manager.fileExists(atPath: url.path)
+//	}
+//
+//	func readDocument(at path: String) -> Data? {
+//		let documentURL = manager.urls(for: .documentDirectory, in: .userDomainMask).first!
+//		let url = documentURL.appendingPathComponent(path)
+//		return try? Data(contentsOf: url)
+//	}
+//}
 
 let file = FileHelper()
 class Teste: Codable {
@@ -37,9 +37,9 @@ struct GroceryProduct: Codable {
     var description: String?
 }
 
-let pear = Teste(teste: "String")
-let encoder = JSONEncoder()
-let data = try encoder.encode(pear)
+//let pear = Teste(teste: "String")
+//let encoder = JSONEncoder()
+//let data = try encoder.encode(pear)
 
 // file.createDocument(named: "cebolas", with: data)
 // print(String(data: data, encoding: .utf8)!)
